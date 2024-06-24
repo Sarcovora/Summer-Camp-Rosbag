49fd5ab4d4550a7884ef9496540702998d3867da
Boueny Folefack <bouenf@cs.utexas.edu>
2024-06-24 15:26:41 -0500

    Initial commit

---
+++ decompress.py
@@ -0,0 +1,7 @@
+import rospy
+from sensor_msgs.msg import CompressedImage
+import cv2
+
+
+def main():
+    subscriber = rospy.Subscriber("")
---
+++ replay_SLAM.bash
@@ -0,0 +1,11 @@
+# roscore >/dev/null 2>&1 &
+source /catkin_ws/devel/setup.bash
+rosparam set use_sim_time true
+roslaunch realsense2_camera opensource_tracking.launch offline:=true > /dev/null 2>&1 &
+echo "Running image_transport"
+rosrun image_transport republish compressed in:=/camera/color/image_raw raw out:=/camera/color/image_raw &
+rosrun image_transport republish compressedDepth in:=/camera/aligned_depth_to_color/image_raw raw out:=/camera/aligned_depth_to_color/image_raw &
+echo "Press Ctrl^C to exit"
+trap 'rosnode kill --all' SIGINT
+rosbag play "$1" --clock
+sleep 3000h
