ea943d41c89a4182573705eb3d66aa71d855173b
Evan Kuo <evan.kuo.edu@gmail.com>
2024-06-27 15:30:37 -0500

    removed cp of openlauncher

--- setup/setup.bash
+++ setup/setup.bash
@@ -1,6 +1,6 @@
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
 fi
 DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -yq --no-install-recommends
 apt-get install -y usbutils
@@ -12,7 +12,7 @@ mkdir -p /etc/apt/keyrings
 apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 apt-get -y install software-properties-common
 add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
-sudo apt-get update --fix-missing 
+sudo apt-get update --fix-missing
 apt-get -y install librealsense2-utils
 
 mkdir -p /catkin_ws/src
@@ -24,7 +24,7 @@ catkin_create_pkg collect_data_pkg std_msgs rospy roscpp sensor_msgs realsense2_
 mkdir -p /catkin_ws/src/collect_data_pkg/launch
 
 # ADD collect_data.py /catkin_ws/src/collect_data_pkg/src
-echo "source /catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source /catkin_ws/devel/setup.bash" >>~/.bashrc
 ./init.bash
 mkdir -p /data
 sudo apt-get install -y ros-noetic-imu-filter-madgwick
@@ -34,7 +34,7 @@ sudo apt-get install -y ros-noetic-octomap-rviz-plugins
 sudo apt-get install -y ros-noetic-compressed-image-transport
 sudo apt-get install -y ros-noetic-compressed-depth-image-transport
 pip install opencv-python
-cp opensource_tracking.launch /opt/ros/noetic/share/realsense2_camera/launch/
+# cp opensource_tracking.launch /opt/ros/noetic/share/realsense2_camera/launch/
 
 # COPY opensource_tracking.launch /opt/ros/noetic/share/realsense2_camera/launch/
 # COPY rs_camera.launch /opt/ros/noetic/share/realsense2_camera/launch/
